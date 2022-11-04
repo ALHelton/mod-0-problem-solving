ca808585e19d3144fe829a1cbabbb0451d583ca5
Andra Helton <andrahelton@gmail.com>
2022-11-04 13:23:05 -0600

    Add changes to challenge titles

---
+++ challenge6.rb
@@ -0,0 +1,46 @@
+# Overall Goal: write a code that returns a string with 
+# a phrase where every word is capitalized
+
+# start with a string that contains a sentence.
+# Make sure that every word in the string is lowercase.
+
+# look at each element with the .each method and 
+# check to see if the first letter of each word is 
+# lowercase
+# use a method that capitalizes the first character of
+# every word
+# it will print each word starting with an uppercase 
+# letter
+
+# activities = ["running", "soccer", "golf", "rowing"]
+
+# activities.each do |activity|
+#     p activity.capitalize!
+# end
+
+# phrase = "this is so fun!"
+
+
+# def capitalize_first
+#      .map(&:capitalize)
+#     end
+# end
+
+phrase = "this is so fun!"
+
+phrase.split
+
+p.phrase.capitalize_first
+
+# def capitalize_first(string)
+#     string.each 
+
+# p phrase.titleize("this is so fun!")
+
+# phrase = "this is so fun!"
+
+# p phrase.split
+
+# p phrase.first.upcase!
+
+# p phrase
